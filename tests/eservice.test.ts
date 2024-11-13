a298109e0b586ece253aff80d71c6a39dcdec2f0
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-11-13 14:26:41 +0100

    Bump version

--- tests/eservice.test.ts
+++ tests/eservice.test.ts
@@ -42,6 +42,7 @@ describe("eservice", () => {
                 verified: [],
                 declared: [],
               },
+              rejectionReasons: [],
             },
           ],
         },
