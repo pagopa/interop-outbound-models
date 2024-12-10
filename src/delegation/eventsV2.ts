9dd9e39b0584c090bfcce8efc519d03ea8e67c58
AsterITA <fabrizio.musella@gmail.com>
2024-12-10 12:03:35 +0100

    added delegationConsumer events

--- src/delegation/eventsV2.ts
+++ src/delegation/eventsV2.ts
@@ -5,6 +5,10 @@ import {
   ProducerDelegationApprovedV2,
   ProducerDelegationRejectedV2,
   ProducerDelegationRevokedV2,
+  ConsumerDelegationApprovedV2,
+  ConsumerDelegationRejectedV2,
+  ConsumerDelegationRevokedV2,
+  ConsumerDelegationSubmittedV2,
 } from "../gen/v2/delegation/events.js";
 import { protobufDecoder } from "../utils.js";
 
@@ -23,6 +27,18 @@ export function delegationEventToBinaryDataV2(
     )
     .with({ type: "ProducerDelegationRevoked" }, ({ data }) =>
       ProducerDelegationRevokedV2.toBinary(data)
+    )
+    .with({ type: "ConsumerDelegationSubmitted" }, ({ data }) =>
+      ConsumerDelegationSubmittedV2.toBinary(data)
+    )
+    .with({ type: "ConsumerDelegationApproved" }, ({ data }) =>
+      ConsumerDelegationApprovedV2.toBinary(data)
+    )
+    .with({ type: "ConsumerDelegationRejected" }, ({ data }) =>
+      ConsumerDelegationRejectedV2.toBinary(data)
+    )
+    .with({ type: "ConsumerDelegationRevoked" }, ({ data }) =>
+      ConsumerDelegationRevokedV2.toBinary(data)
     )
     .exhaustive();
 }
@@ -56,6 +72,38 @@ export const DelegationEventV2 = z.discriminatedUnion("type", [
     event_version: z.literal(2),
     type: z.literal("ProducerDelegationRevoked"),
     data: protobufDecoder(ProducerDelegationRevokedV2),
+    stream_id: z.string(),
+    version: z.number(),
+    timestamp: z.coerce.date(),
+  }),
+  z.object({
+    event_version: z.literal(2),
+    type: z.literal("ConsumerDelegationSubmitted"),
+    data: protobufDecoder(ConsumerDelegationSubmittedV2),
+    stream_id: z.string(),
+    version: z.number(),
+    timestamp: z.coerce.date(),
+  }),
+  z.object({
+    event_version: z.literal(2),
+    type: z.literal("ConsumerDelegationApproved"),
+    data: protobufDecoder(ConsumerDelegationApprovedV2),
+    stream_id: z.string(),
+    version: z.number(),
+    timestamp: z.coerce.date(),
+  }),
+  z.object({
+    event_version: z.literal(2),
+    type: z.literal("ConsumerDelegationRejected"),
+    data: protobufDecoder(ConsumerDelegationRejectedV2),
+    stream_id: z.string(),
+    version: z.number(),
+    timestamp: z.coerce.date(),
+  }),
+  z.object({
+    event_version: z.literal(2),
+    type: z.literal("ConsumerDelegationRevoked"),
+    data: protobufDecoder(ConsumerDelegationRevokedV2),
     stream_id: z.string(),
     version: z.number(),
     timestamp: z.coerce.date(),
