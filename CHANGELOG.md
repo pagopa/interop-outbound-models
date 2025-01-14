8aa3177f9a6b9bcf676649e2e2c9c5138de35dad
Roberto Taglioni <roberto.taglioni@pagopa.it>
2025-01-14 10:24:07 +0100

    Update outbound models

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,8 @@
 
 All notable changes to this project will be documented in this file.
 
-## 1.0.13
+## 1.0.14
 
 ### Added
-- Added e-service event `EServiceDescriptorAttributesUpdated` (In-Add Attributes Feature)
+
+- Added e-service event `EServiceNameUpdated`
--- src/eservice/eventsV2.ts
+++ src/eservice/eventsV2.ts
@@ -22,6 +22,7 @@ import {
   EServiceDescriptorQuotasUpdatedV2,
   EServiceDescriptionUpdatedV2,
   EServiceDescriptorAttributesUpdatedV2,
+  EServiceNameUpdatedV2,
 } from "../gen/v2/eservice/events.js";
 
 export function eServiceEventToBinaryDataV2(
@@ -87,6 +88,9 @@ export function eServiceEventToBinaryDataV2(
     )
     .with({ type: "EServiceDescriptorAttributesUpdated" }, ({ data }) =>
       EServiceDescriptorAttributesUpdatedV2.toBinary(data)
+    )
+    .with({ type: "EServiceNameUpdated" }, ({ data }) =>
+      EServiceDescriptionUpdatedV2.toBinary(data)
     )
     .exhaustive();
 }
@@ -248,6 +252,14 @@ export const EServiceEventV2 = z.discriminatedUnion("type", [
     event_version: z.literal(2),
     type: z.literal("EServiceDescriptorAttributesUpdated"),
     data: protobufDecoder(EServiceDescriptorAttributesUpdatedV2),
+    stream_id: z.string(),
+    version: z.number(),
+    timestamp: z.coerce.date(),
+  }),
+  z.object({
+    event_version: z.literal(2),
+    type: z.literal("EServiceNameUpdated"),
+    data: protobufDecoder(EServiceNameUpdatedV2),
     stream_id: z.string(),
     version: z.number(),
     timestamp: z.coerce.date(),
