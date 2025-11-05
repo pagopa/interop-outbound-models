8b271c76025f68843495e8d4547ca40990cdc808
Vittorio Caprio <vittorio.caprio@buildo.io>
2025-11-05 16:29:59 +0100

    Add export to  purpose template events (#51)
    * feature: Update outbound models to version 1.8 adding purpose template events (PIN-7976)
    
    * feature: add properly export of generated events(PIN-7976)
    
    * feature: removing RiskAnalysisFormTemplateV2  from event definitio (PIN-7976)
    
    * update to version 1.8.1, update changelog
    
    ---------
    
    Co-authored-by: Paolo Manca <paolo.manca@pagopa.it>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./purpose/index.js";
 export * from "./tenant/index.js";
 export * from "./delegation/index.js";
 export * from "./eservice-template/index.js";
+export * from "./purpose-template/index.js";
 
 export * from "./gen/v1/agreement/agreement.js";
 export * from "./gen/v1/agreement/events.js";
@@ -26,3 +27,5 @@ export * from "./gen/v2/delegation/delegation.js";
 export * from "./gen/v2/delegation/events.js";
 export * from "./gen/v2/eservice-template/eservice-template.js";
 export * from "./gen/v2/eservice-template/events.js";
+export * from "./gen/v2/purpose-template/purpose-template.js";
+export * from "./gen/v2/purpose-template/events.js";
